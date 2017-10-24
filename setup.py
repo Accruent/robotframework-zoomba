5519c5c981ce7cfe72974243016629d820e86c53
Alexander Calandra <acalandra@accruent.com>
2017-10-24 13:22:55 -0500

    Adding Setup.py

---
+++ setup.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import os
+import pip
+import sys
+from os.path import join, dirname
+
+sys.path.append(join(dirname(__file__), 'src'))
+from ez_setup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+VERSION = """
+1.0.0
+""".strip()
+
+DESCRIPTION = """
+Zoomba
+""".strip()
+
+CLASSIFIERS  = """
+Development Status :: 5 - Production/Stable
+Operating System :: OS Independent
+Programming Language :: Python
+Topic :: Software Development :: Communication
+""".strip().splitlines()
+
+setup(name         = 'zoomba',
+      version      = VERSION,
+      description  = 'Robot Framework mini-framework.',
+      long_description = DESCRIPTION,
+      url          = 'https://github.com/Accruent/zoomba',
+      license      = '',
+      keywords     = 'bots',
+      platforms    = 'any',
+      install_requires=[
+      ],
+      classifiers  = CLASSIFIERS,
+      zip_safe     = True,
+      packages     = ['zoomba']
+      )
