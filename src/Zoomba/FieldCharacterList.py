ef44d30d6d5125ee00081a64988c0b108e898401
Alexander Calandra <calandra.aj@gmail.com>
2017-11-14 10:54:54 -0600

    Adding FieldCharacters

---
+++ src/Zoomba/FieldCharacterList.py
@@ -0,0 +1,130 @@
+Forty = "FortyFortyFortyFortyFortyFortyFortyForty"
+
+One_Hundred_Ninety_Nine = "OneHundredNinetyNineOneHundredNinetyNineOneHundredNinetyNineOneHundredNinetyNineOne" \
+                          "HundredNinetyNineOneHundredNinetyNineOneHundredNinetyNineOneHundredNinetyNineOneHundred" \
+                          "NinetyNineOneHundredNinetyNin"
+
+Two_Hundred = "TwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwo" \
+              "HundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundredTwoHundred" \
+              "TwoHundred"
+
+Two_Hundred_One = "TwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOne" \
+                  "TwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOneTwoHundredOne" \
+                  "TwoHundredOneTwoHun"
+
+One_Thousand_Nine_Hundred_Ninety_Eight = "OneThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHundredNinteyEightOneThousandNineHundredNinteyEightOne" \
+                                         "ThousandNineHun"
+
+One_Thousand_Nine_Hundred_Ninety_Nine = "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNine" \
+                                        "OneThousandNineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousand" \
+                                        "NineHundredNinteyNineOneThousandNineHundredNinteyNineOneThousandNineHundred" \
+                                        "NinteyNineOneThousandNineHundredNinteyNineOneThousandNine"
+
+Two_Thousand = "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousandTwoThousand" \
+               "TwoThousandTwoThousa"
+
+Two_Thousand_One = "TwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousand" \
+                   "OneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwo" \
+                   "ThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOne" \
+                   "TwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousand" \
+                   "OneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwo" \
+                   "ThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOne" \
+                   "TwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousand" \
+                   "OneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwo" \
+                   "ThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOne" \
+                   "TwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousand" \
+                   "OneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwo" \
+                   "ThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOne" \
+                   "TwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousand" \
+                   "OneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwo" \
+                   "ThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOne" \
+                   "TwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousand" \
+                   "OneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwo" \
+                   "ThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOne" \
+                   "TwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousand" \
+                   "OneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwo" \
+                   "ThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOneTwoThousandOne" \
+                   "TwoThousandOneTwoThousandOneTwoThousandOn"
+
+ASCII = """! "#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~"""
+
+ASCII_Short_1 = """! "#$%&'()*+,-./0123456789:;<=>?@ABCDEFG"""
+
+ASCII_Short_2 = """HIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmno"""
+
+ASCII_Short_3 = "pqrstuvwxyz{|}~"
+
+SQL_Injection = "select * from ADM_USERS where id = 1 or 1=1;"
+
+XSS = """<script>alert("hi")</script>"""
