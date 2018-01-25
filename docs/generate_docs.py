926195692c4951c0f70059af0282402696b9c41b
Alexander Calandra <calandra.aj@gmail.com>
2018-01-25 15:56:11 -0600

    \\ -> /

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -4,6 +4,6 @@ from robot import libdoc
 libraries = ['APILibrary.py', 'GUILibrary.py', 'SOAPLibrary.py']
 curr_dir = os.path.dirname(os.path.realpath(__file__)).replace('docs', '')
 for file in libraries:
-    libdoc.libdoc(curr_dir+'src\\Zoomba\\'+file, curr_dir+'docs\\'+file.replace('.py', 'Documentation.html'),
+    libdoc.libdoc(curr_dir+'src/Zoomba/'+file, curr_dir+'docs/'+file.replace('.py', 'Documentation.html'),
                   name=file.replace(".py", "").replace("Lib", "_Lib"))
 
