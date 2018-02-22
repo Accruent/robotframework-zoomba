a5b2bf506136e39f12d142150f47ef8b37ff5c11
Alexander Calandra <calandra.aj@gmail.com>
2018-02-22 17:37:29 -0600

    adding checks for insecure request causing disable_warnings to be called correctly

--- test/API/test_calls.py
+++ test/API/test_calls.py
@@ -32,7 +32,7 @@ class TestExternal(unittest.TestCase):
 
     @patch('RequestsLibrary.RequestsKeywords.create_session')
     @patch('RequestsLibrary.RequestsKeywords.get_request')
-    def test_basic(self, create_session, get_request):
+    def test_basic_get(self, create_session, get_request):
         library = APILibrary()
         r = library.call_get_request({"a": "Text"}, "Endpoint", "fullstring")
         type(r).text = PropertyMock(return_value="success")
@@ -40,3 +40,34 @@ class TestExternal(unittest.TestCase):
         assert r.text
         assert r.status_code == 200
 
+    @patch('RequestsLibrary.RequestsKeywords.create_session')
+    @patch('RequestsLibrary.RequestsKeywords.get_request')
+    @patch('requests.packages.urllib3.disable_warnings')
+    def test_get_insecure_request(self, create_session, get_request, disable_warnings):
+        library = APILibrary()
+        library.suppress_insecure_request_warnings()
+        library.call_get_request({"a": "Text"}, "Endpoint", "fullstring")
+        assert disable_warnings.called
+
+    def test_post_default(self):
+        library = APILibrary()
+        self.assertRaises(TypeError, library.call_post_request)
+
+    @patch('RequestsLibrary.RequestsKeywords.create_session')
+    @patch('RequestsLibrary.RequestsKeywords.post_request')
+    def test_basic_post(self, create_session, get_request):
+        library = APILibrary()
+        r = library.call_post_request({"a": "Text"}, "Endpoint", "fullstring")
+        type(r).text = PropertyMock(return_value="success")
+        type(r).status_code = PropertyMock(return_value=200)
+        assert r.text
+        assert r.status_code == 200
+
+    @patch('RequestsLibrary.RequestsKeywords.create_session')
+    @patch('RequestsLibrary.RequestsKeywords.post_request')
+    @patch('requests.packages.urllib3.disable_warnings')
+    def test_post_insecure_request(self, create_session, post_request, disable_warnings):
+        library = APILibrary()
+        library.suppress_insecure_request_warnings()
+        r = library.call_post_request({"a": "Text"}, "Endpoint", "fullstring")
+        assert disable_warnings.called
