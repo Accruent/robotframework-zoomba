a4c6f27055d7033bb097d1e609ec0246fc195442
MichaelHintz <mhintz@accruent.com>
2018-02-28 17:01:23 -0600

    Adding files.

--- setup.py
+++ setup.py
@@ -41,7 +41,8 @@ setup(name         = 'robotframework-zoomba',
           "robotframework-seleniumlibrary==3.0.1",
           "robotframework-sudslibrary-aljcalandra",
           "requests==2.18.4",
-          "selenium==3.8.1"
+          "selenium==3.8.1",
+          "python-dateutil"
       ],
       classifiers  = CLASSIFIERS,
       package_dir  = {'' : 'src'},
