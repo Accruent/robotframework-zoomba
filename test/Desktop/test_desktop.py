9cf490c26e775e7b1a1d272437b7077dc2b8ebb0
Brandon Wolfe <Wolfe1@gmail.com>
2019-07-16 11:18:05 -0500

    Yeah not sure how to do to mock this part...

--- test/Desktop/test_desktop.py
+++ test/Desktop/test_desktop.py
@@ -16,23 +16,22 @@ class TestInternal(unittest.TestCase):
 
     def test_maximize_window_successful(self):
         am = DesktopLibrary()
-        am.maximize_window = MagicMock(return_value=True)
         appium.webdriver.Remote = WebdriverRemoteMock
         self.assertFalse(am._cache.current)
         am.open_application('remote_url')
         self.assertTrue(am._cache.current)
         self.assertTrue(am.maximize_window())
 
-    # @patch('DesktopLibrary.open_application')
-    def test_open_application_splash_successful(self):
-        am = DesktopLibrary()
-        appium.webdriver.Remote = WebdriverRemoteMock
-        am.open_application = MagicMock(side_effect=Exception)
-        # am.open_application = MagicMock(side_effect=Exception())
-        # am.open_application.side_effect = Exception()
-        self.assertRaises(Exception, am.open_application)
-        # with am.raises(Exception()) as e:
-        #     self.assertEqual(Exception(), e)
-        # self.assertEqual(am.open_application('remote_url', 'window_name=test'), 'WebDriverException')
-
-        # self.assertTrue(am._cache.current)
+    # def test_open_application_splash_successful(self):
+    #     am = DesktopLibrary()
+    #     appium.webdriver.Remote = WebdriverRemoteMock
+    #     # am.open_application = MagicMock(side_effect=Exception)
+    #     # am.open_application = MagicMock(side_effect=Exception())
+    #     # am.open_application.side_effect = Exception()
+    #     am.open_application('zzzz', window_name='test')
+    #     # self.assertRaises(KeyError, am.open_application, 'zzzz', window_name='test')
+    #     # with am.raises(Exception()) as e:
+    #     #     self.assertEqual(Exception(), e)
+    #     # self.assertEqual(am.open_application('remote_url', 'window_name=test'), 'WebDriverException')
+    #
+    #     # self.assertTrue(am._cache.current)
