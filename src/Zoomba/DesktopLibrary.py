fc1b323628895dd3dc3b48a322170bfd78613535
Brandon Wolfe <Wolfe1@gmail.com>
2019-07-17 09:25:16 -0500

    Lets give this a try

--- src/Zoomba/DesktopLibrary.py
+++ src/Zoomba/DesktopLibrary.py
@@ -24,27 +24,26 @@ class DesktopLibrary(AppiumLibrary):
         Examples:
         | Open Application | http://localhost:4723/wd/hub | alias=Myapp1         | platformName=Windows            | deviceName=Windows           | app=your.app          |
         """
-        try:
-            desired_caps = kwargs
-            # global application
-            application = webdriver.Remote(str(remote_url), desired_caps)
-        except:
+        desired_caps = kwargs
+        if window_name:
             """ 
-            If the app has a splash screen the above code will start the application but will fail. This is because
-            the application it opened is no longer available. The next piece of code searches the desktop for the 
-            created window and attaches the webdriver to it.
+            If the app has a splash screen we need to supply the window_name of the final window. This code path will 
+            start the application and then attach to the correct window via the window_name.
             """
-            if window_name:
-                desktop_capabilities = dict()
-                desktop_capabilities.update({"app": "Root", "platformName": "Windows", "deviceName": "WindowsPC"})
-                desktop_session = webdriver.Remote(str(remote_url), desktop_capabilities)
-                window = desktop_session.find_element_by_name(window_name)
-                window = hex(int(window.get_attribute("NativeWindowHandle")))
-                if "app" in desired_caps:
-                    del desired_caps["app"]
-                desired_caps["appTopLevelWindow"] = window
-                application = webdriver.Remote(str(remote_url), desired_caps)
-
+            try:
+                webdriver.Remote(str(remote_url), desired_caps)
+            except:
+                pass
+            desktop_capabilities = dict()
+            desktop_capabilities.update({"app": "Root", "platformName": "Windows", "deviceName": "WindowsPC"})
+            desktop_session = webdriver.Remote(str(remote_url), desktop_capabilities)
+            window = desktop_session.find_element_by_name(window_name)
+            window = hex(int(window.get_attribute("NativeWindowHandle")))
+            if "app" in desired_caps:
+                del desired_caps["app"]
+            desired_caps["appTopLevelWindow"] = window
+        # global application
+        application = webdriver.Remote(str(remote_url), desired_caps)
         self._debug('Opened application with session id %s' % application.session_id)
 
         return self._cache.register(application, alias)
