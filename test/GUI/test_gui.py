d852c892c2de3d8afcb13692e57091fa93fbfd32
Brandon Wolfe <Wolfe1@gmail.com>
2019-09-03 11:06:35 -0500

    lets see if that adds it back to coverage

--- test/GUI/test_gui.py
+++ test/GUI/test_gui.py
@@ -3,6 +3,9 @@ import unittest
 from unittest.mock import patch
 from unittest.mock import Mock
 from unittest.mock import PropertyMock
+import os
+import sys
+sys.path.insert(0, os.path.abspath( os.path.join(os.path.dirname(__file__), '../../src/')))
 
 
 class TestInternal(unittest.TestCase):
