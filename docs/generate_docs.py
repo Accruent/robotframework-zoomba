6210ed7adce9472b07ead1021dd4b0da4ecb4ae5
Brandon Wolfe <Wolfe1@gmail.com>
2020-01-22 14:13:01 -0600

    send to PR for testing

--- docs/generate_docs.py
+++ docs/generate_docs.py
@@ -2,7 +2,7 @@ import os
 from robot import libdoc
 from version import VERSION
 
-libraries = ['APILibrary.py', 'GUILibrary.py', 'SOAPLibrary.py', 'DesktopLibrary.py']
+libraries = ['APILibrary.py', 'GUILibrary.py', 'SOAPLibrary.py', 'DesktopLibrary.py', 'MobileLibrary.py']
 curr_dir = os.path.dirname(os.path.realpath(__file__)).replace('docs', '')
 for file in libraries:
     libdoc.libdoc(curr_dir+'src/Zoomba/'+file, curr_dir+'docs/'+file.replace('.py', 'Documentation.html'),
