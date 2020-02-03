15db0f300b23693160872754a57cb3afc1944a07
Brandon Wolfe <Wolfe1@gmail.com>
2020-02-03 13:29:26 -0600

    Use readme as the long description in pypi

--- setup.py
+++ setup.py
@@ -7,15 +7,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+with open("docs/README.md", "r") as fh:
+    long_description = fh.read()
+
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setup(name='robotframework-zoomba',
       version=version.VERSION,
       description='Robot Framework mini-framework.',
-      long_description='Zoomba',
+      long_description=long_description,
       url='https://github.com/Accruent/zoomba',
-      maintainer='Alex Calandra, Michael Hintz, Keith Smoland, Matthew Giardina, Brandon Wolfe, Neil Howell, Tommy Hoang',
+      maintainer='Alex Calandra, Michael Hintz, Keith Smoland, Matthew Giardina, Brandon Wolfe, Neil Howell,'
+                 ' Tommy Hoang',
       maintainer_email='robosquad@accruent.com',
       license='GPL-3.0',
       keywords='Robot Framework robot-framework selenium requests appium soap winappdriver appium robotframework'
