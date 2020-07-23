83aaca268b81345ff2a153af3f436594f8dde3dd
ACCRUENT\michael.nester <michael.nester@accruent.com>
2020-07-23 18:20:16 -0500

    react_select: Saving progress on adding React Helpers.

--- src/Zoomba/GUILibrary.py
+++ src/Zoomba/GUILibrary.py
@@ -5,6 +5,7 @@ from robot.api.deco import keyword
 from robot.libraries.Collections import Collections
 from time import time
 from robot.utils import is_string
+from Zoomba.ReactHelpers import ReactHelper
 
 zoomba = BuiltIn()
 zoomba_collections = Collections()
@@ -334,3 +335,13 @@ class GUILibrary(SeleniumLibrary):
             timestamp = time()
             filename = 'selenium-screenshot-' + str(timestamp) + '-' + str(next(SCREENSHOT_COUNTER)) + '.png'
             return self.capture_page_screenshot(filename)
+
+    @keyword("Get React List Items")
+    def get_react_list_items(self, locator, values=False):  # ToDo: Fix ModuelNotFoundError when running from automation-framework-360. Only happnes when running from *.robot file. Python files are good to go.
+        test_variable = ReactHelper.ReactHelper(self)
+        test_variable.tester('HellO!')
+        #options = self._get_options(locator, 'div')
+        #if is_truthy(values):
+        #    return self._get_values(options)
+        #else:
+        #    return self._get_labels(options)
---
+++ src/Zoomba/ReactHelpers/ReactSelect.py
@@ -0,0 +1,69 @@
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, UnexpectedTagNameException
+
+
+class ReactSelect(Select):  # ToDo: Update documentation
+
+    def __init__(self, webelement):
+        """
+        Constructor. A check is made that the given element is, indeed, a SELECT tag. If it is not,
+        then an UnexpectedTagNameException is thrown.
+
+        :Args:
+         - webelement - element SELECT element to wrap
+
+        Example:
+            from selenium.webdriver.support.ui import Select \n
+            Select(driver.find_element_by_tag_name("select")).select_by_index(2)
+        """
+        if webelement.tag_name.lower() != "div":
+            raise UnexpectedTagNameException(
+                "ReactSelect only works on <div> elements, not on <%s>" %
+                webelement.tag_name)
+        self._el = webelement
+        multi = self._el.get_attribute("multiple")
+        self.is_multiple = multi and multi != "false"
+
+    @property
+    def options(self):
+        """Returns a list of all options belonging to this div tag"""
+        return self._el.find_elements(By.TAG_NAME, 'div')
+
+    def select_by_visible_text(self, text):
+        """Select all options that display text matching the argument. That is, when given "Bar" this
+           would select an option like:
+
+            <option value="foo">Bar</option>
+
+           :Args:
+            - text - The visible text to match against
+
+            throws NoSuchElementException If there is no option with specified text in SELECT
+           """
+        xpath = ".//div[normalize-space(.) = %s]" % self._escapeString(text)
+        opts = self._el.find_elements(By.XPATH, xpath)
+        matched = False
+        for opt in opts:
+            self._setSelected(opt)
+            if not self.is_multiple:
+                return
+            matched = True
+
+        if len(opts) == 0 and " " in text:
+            subStringWithoutSpace = self._get_longest_token(text)
+            if subStringWithoutSpace == "":
+                candidates = self.options
+            else:
+                xpath = ".//div[contains(.,%s)]" % self._escapeString(subStringWithoutSpace)
+                candidates = self._el.find_elements(By.XPATH, xpath)
+            for candidate in candidates:
+                if text == candidate.text:
+                    self._setSelected(candidate)
+                    if not self.is_multiple:
+                        return
+                    matched = True
+
+        if not matched:
+            raise NoSuchElementException("Could not locate element with visible text: %s" % text)
+
