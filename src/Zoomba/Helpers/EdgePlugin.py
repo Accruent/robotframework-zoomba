f0cd33a346f889bdced0a09fd88255f1991ae452
Brandon Wolfe <Wolfe1@gmail.com>
2021-06-22 09:27:19 -0500

    Web view2 (#207)
    * 'Refactored by Sourcery'
    
    * Initial test of WebView2 support
    
    * omit some files just used to bridge us to selenium 4
    
    * Refactor, no new keyword needed
    
    Co-authored-by: Sourcery AI <>

--- src/Zoomba/Helpers/EdgePlugin.py
+++ src/Zoomba/Helpers/EdgePlugin.py
@@ -4,6 +4,9 @@ from SeleniumLibrary import BrowserManagementKeywords
 from SeleniumLibrary.utils import is_truthy, is_falsy
 from SeleniumLibrary.keywords.webdrivertools import WebDriverCreator, SeleniumOptions
 from msedge.selenium_tools import Edge as EdgePluginDriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from .chromeOptions import ChromiumOptions
+from robot.api.deco import keyword
 
 
 class EdgePlugin(BrowserManagementKeywords):
@@ -17,6 +20,10 @@ class EdgePlugin(BrowserManagementKeywords):
     def __init__(self, ctx):
         BrowserManagementKeywords.__init__(self, ctx)
         self._webdriver_creator = _EdgePluginWebDriverCreator(self.log_dir)
+
+    @keyword
+    def get_edge_options(self):
+        return Options()
 
 
 class _EdgePluginWebDriverCreator(WebDriverCreator):
@@ -46,3 +53,51 @@ class _EdgePluginSeleniumOptions(SeleniumOptions):
             options = importlib.import_module('msedge.selenium_tools.options')
             return options.Options
         return super(_EdgePluginSeleniumOptions, self)._import_options(browser)
+
+
+# Pulled from selenium 4 beta for now as AppiumLibrary is not compatible with it yet
+
+class Options(ChromiumOptions):
+    KEY = "ms:edgeOptions"
+
+    def __init__(self):
+        super(Options, self).__init__()
+        self._use_chromium = True
+        self._use_webview = False
+
+    @property
+    def use_chromium(self) -> bool:
+        return self._use_chromium
+
+    @use_chromium.setter
+    def use_chromium(self, value: bool):
+        self._use_chromium = bool(value)
+
+    @property
+    def use_webview(self) -> bool:
+        return self._use_webview
+
+    @use_webview.setter
+    def use_webview(self, value: bool):
+        self._use_webview = bool(value)
+
+    def to_capabilities(self) -> dict:
+        """
+        Creates a capabilities with all the options that have been set and
+        :Returns: A dictionary with everything
+        """
+        caps = self._caps
+
+        if self._use_chromium:
+            caps = super(Options, self).to_capabilities()
+            if self._use_webview:
+                caps['browserName'] = 'webview2'
+        else:
+            caps['platform'] = 'windows'
+
+        caps['ms:edgeChromium'] = self._use_chromium
+        return caps
+
+    @property
+    def default_capabilities(self) -> dict:
+        return DesiredCapabilities.EDGE.copy()
