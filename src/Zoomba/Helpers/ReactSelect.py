76c031c8e9288325a2842e4b4333b39fe79e2000
Brandon Wolfe <Wolfe1@gmail.com>
2022-06-24 10:22:01 -0500

    missed one
    Signed-off-by: Brandon Wolfe <Wolfe1@gmail.com>

--- src/Zoomba/Helpers/ReactSelect.py
+++ src/Zoomba/Helpers/ReactSelect.py
@@ -37,7 +37,7 @@ class ReactSelect:
     def options(self):
         """Returns a list of all options belonging to the React-Select container"""
         self.expand_select_list()
-        return self._el.find_elements_by_xpath('./div[2]/div[1]/div')
+        return self._el.find_elements(By.XPATH, './div[2]/div[1]/div')
 
     def is_expanded(self):
         """Checks if the React-Select container is expanded by checking if the Menu <div>
