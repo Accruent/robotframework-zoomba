3fcfe7b5e6d56495eadf10247f34cc730b2aa3a8
Brandon Wolfe <Wolfe1@gmail.com>
2022-06-28 07:52:31 -0500

    Warn users that the libraries have moved to robotframework-applicationlibrary (#288)
    * Warn users that the libraries have moved to robotframework-applicationlibrary
    
    Signed-off-by: Brandon Wolfe <Wolfe1@gmail.com>
    
    * ignore deprecated classes
    
    Signed-off-by: Brandon Wolfe <Wolfe1@gmail.com>
    
    * newline
    
    Signed-off-by: Brandon Wolfe <Wolfe1@gmail.com>

---
+++ src/Zoomba/DesktopLibrary.py
@@ -0,0 +1,2 @@
+class DesktopLibrary:
+    raise DeprecationWarning('This code has been moved to Robotframework-ApplicationLibrary as of version 3.0.0, more information: https://github.com/Accruent/robotframework-zoomba/releases/tag/3.0.0')
