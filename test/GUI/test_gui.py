c37112254af903c604958f17cca4a8c53588af14
Brandon Wolfe <Wolfe1@gmail.com>
2023-11-08 14:12:08 -0600

    fix

--- test/GUI/test_gui.py
+++ test/GUI/test_gui.py
@@ -245,9 +245,10 @@ class TestInternal(unittest.TestCase):
         mock_gui.switch_window.assert_called_with("title")
 
     @patch('robot.libraries.BuiltIn.BuiltIn.sleep')
-    def test_wait_until_javascript_is_complete_and_page_load(self, robot_call):
+    def test_wait_until_javascript_is_complete(self, robot_call):
         mock_gui = Mock()
-        mock_gui.execute_javascript = Mock(side_effect=[False, True, False, True])
+        mock_gui.execute_javascript = Mock(side_effect=["loading", False, "complete", False,
+                                                        "complete", True, "complete", True])
         GUILibrary.wait_until_javascript_is_complete(mock_gui)
         robot_call.assert_called()
 
