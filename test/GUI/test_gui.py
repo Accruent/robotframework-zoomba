ba0233a0d871839ce523718d0a2aea3abcd9479a
Brandon Wolfe <Wolfe1@gmail.com>
2023-11-08 14:25:47 -0600

    for unit test coverage

--- test/GUI/test_gui.py
+++ test/GUI/test_gui.py
@@ -253,6 +253,14 @@ class TestInternal(unittest.TestCase):
         robot_call.assert_called()
 
     @patch('robot.libraries.BuiltIn.BuiltIn.sleep')
+    def test_wait_until_javascript_is_complete_exception_raised(self, robot_call):
+        mock_gui = Mock()
+        mock_gui.execute_javascript = Mock(side_effect=Exception("Oh No!"))
+        GUILibrary.wait_until_javascript_is_complete(mock_gui)
+        with self.assertRaises(Exception, msg="Oh No!"):
+            GUILibrary.wait_until_javascript_is_complete(mock_gui)
+
+    @patch('robot.libraries.BuiltIn.BuiltIn.sleep')
     def test_wait_for_page_load(self, robot_call):
         mock_gui = Mock()
         mock_gui.execute_javascript = Mock(side_effect=["loading", "loading", "loading", "complete"])
